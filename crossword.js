5a302039219b605a1c92ee91a18a50e567fa9e68
Patrick Nance <jpnance@gmail.com>
2019-02-05 20:13:32 -0800

    Awesome. Here's some basic vim navigation.

--- crossword.js
+++ crossword.js
@@ -3,10 +3,51 @@ var editor = {
 
 	grid: {
 		height: 15,
-		width: 15
+		width: 15,
+
+		cursorPosition: 0
 	},
 
 	keyHandler: (e) => {
+		console.log(e.keyCode);
+
+		if (editor.mode == 'command') {
+			switch (e.keyCode) {
+				// j is for 'up'
+				case 74:
+					if (editor.grid.cursorPosition + editor.grid.height < editor.grid.height * editor.grid.width) {
+						editor.grid.cursorPosition += editor.grid.height;
+					}
+
+					break;
+
+				// k is for 'down'
+				case 75:
+					if (editor.grid.cursorPosition - editor.grid.height >= 0) {
+						editor.grid.cursorPosition -= editor.grid.height;
+					}
+
+					break;
+
+				// h is for 'left'
+				case 72:
+					if (editor.grid.cursorPosition % editor.grid.width > 0) {
+						editor.grid.cursorPosition -= 1;
+					}
+
+					break;
+
+				// l is for 'right'
+				case 76:
+					if (editor.grid.cursorPosition % editor.grid.width < editor.grid.width - 1) {
+						editor.grid.cursorPosition += 1;
+					}
+
+					break;
+			}
+		}
+
+		editor.renderGrid();
 	},
 
 	renderGrid: () => {
@@ -17,6 +58,10 @@ var editor = {
 
 			for (var j = 0; j < editor.grid.width; j++) {
 				var gridCell = $('<div>').addClass('cell');
+
+				if (i * editor.grid.height + j == editor.grid.cursorPosition) {
+					gridCell.addClass('cursor');
+				}
 
 				gridRow.append(gridCell);
 			}
@@ -30,5 +75,6 @@ var editor = {
 
 $(document).ready(function() {
 	editor.renderGrid();
+
 	$('body').on('keydown', editor.keyHandler);
 });
